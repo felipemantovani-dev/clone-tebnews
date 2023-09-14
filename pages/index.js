ff9b2dd44d02ed35c08ea9e19b8b403c0f02a4fd
Felipe Santhiago Mantovani <144163629+felipemantovani-dev@users.noreply.github.com>
2023-09-14 18:18:50 +0000

    add the index page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function home () {
+    return <h1>Iae, o professor pediu, como desafio, para que a gente lancasse essa página na internet, com meu próprio DNS, e de alguma forma impactasse alguém, pois então eu escolhi você kkk
+        queria que você vesse que estou aprendendo. Estou gostando de mais desse curso.
+        É isso, vlw :)
+    </h1>
+}
+
+export default home
