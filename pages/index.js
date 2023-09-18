fcdd55980757d23e3f40df16cdf3cabe1702c05e
Felipe Santhiago Mantovani <144163629+felipemantovani-dev@users.noreply.github.com>
2023-09-18 18:11:21 +0000

    add the smile emoji on phrase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function home () {
     return <h1>Iae, o professor pediu, como desafio, para que a gente lancasse essa página na internet, com meu próprio DNS, e de alguma forma impactasse alguém, pois então eu escolhi você kkk
         queria que você vesse que estou aprendendo. Estou gostando de mais desse curso.
-        É isso, vlw :)
+        É isso, vlw 😁
     </h1>
 }
 
