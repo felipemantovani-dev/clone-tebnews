b927475aff6d9d9680b42d319ad38f349edd7a9e
Felipe Santhiago Mantovani <144163629+felipemantovani-dev@users.noreply.github.com>
2023-09-18 18:39:44 +0000

    modify the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function home () {
-    return <h1>Iae, o professor pediu, como desafio, para que a gente lancasse essa página na internet, com meu próprio DNS, e de alguma forma impactasse alguém, pois então eu escolhi você kkk
+    return <h1>Iae, o professor pediu, como desafio, para que a gente fizesse o DEPLOY da página, e de alguma forma impactasse alguém, pois então eu escolhi você kkk
         queria que você vesse que estou aprendendo. Estou gostando de mais desse curso.
+        Até agora aprendi a importânica do VERSIONAMENTO e do REPOSITÓRIO, aprendi a diferença do GIT e do GIT HUB, assim como aprendi a usa-los, além de vários bom hábitos e modos que um bom programador deve ter.
         É isso, vlw 😁
     </h1>
 }
